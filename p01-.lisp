dbcab1f63fb951ee07b636f9659da9e7a9fb7d8a
iwashiKun <shinji.akasaka@gmail.com>
2023-07-17 21:15:06 +0900

    first committ.

---
+++ p01-.lisp
@@ -0,0 +1,30 @@
+;; L-99
+;; https://www.ic.unicamp.br/~meidanis/courses/mc336/problemas-lisp/L-99_Ninety-Nine_Lisp_Problems.html
+;
+;; p01-
+
+;; l99-p01
+(defun l99-p01 (lst)
+  (if (equal (length lst) 1)
+      lst
+      (l99-p01 (cdr lst))))
+
+;; l99-p02
+(defun l99-p02 (lst)
+  (if (equal (length lst) 2)
+      lst
+      (l99-p02 (cdr lst))))
+
+;; l99-p03
+(defun l99-p03 (lst pos)
+  (if (equal pos 1)
+      (car lst)
+      (l99-p03 (cdr lst) (decf pos))))
+
+;; l99-p04
+(defun l99-p04 (lst lng)
+  (if (equal '() lst)
+      lng
+      (progn
+        (incf lng)
+        (l99-p04 (cdr lst) lng))))
